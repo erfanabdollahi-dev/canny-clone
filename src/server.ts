1076f2c537a94157774dff2d80480ffb5a6201f1
erfanabdollahi <erfanabdollahi.dev@gmail.com>
2026-07-31 21:52:01 +0330

    feat: add bootstrap for the server boot steps

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-import app from './app.js';
+import app from "./app.js";
+import connectDatabase from "@/database/connection.js";
+import env from "@/config/env.js";
+
+const bootstrap = async () => {
+  try {
+    await connectDatabase();
 
 
-app.listen(5000, ()=>{
-  console.log('server started : http://127.0.0.1:5000')
-})
+    app.listen(Number(env.PORT), () => {
+      console.log(`Server Started : http://127.0.0.1:${Number(env.PORT)}`);
+    });
+
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+bootstrap();
